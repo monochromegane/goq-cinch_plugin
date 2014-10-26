7a620e347f370c03a4c64162afe3345dd4af77f1
monochromegane <dev.kuro.obi@gmail.com>
2014-10-26 11:12:49 +0900

    Call goq query.

--- lib/goq/cinch_plugin.rb
+++ lib/goq/cinch_plugin.rb
@@ -4,7 +4,13 @@ module Goq
   class CinchPlugin
     include Cinch::Plugin
 
+    match(/^goq (.+)/, method: :query, use_prefix: false)
     match(/^goq list/, method: :list, use_prefix: false)
+
+    def query(m, query)
+      result = `goq test #{query}`
+      m.reply result
+    end
 
     def list(m)
       list = `goq --list test`
